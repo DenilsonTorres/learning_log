0210f5c9dccf46e88d143970891f7b4e839f8152
Denilson Torres <dtlima2020@gmail.com>
2024-06-10 11:55:07 -0300

    Learning_log App

---
+++ learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Criação de Topicos
+class Topic(models.Model):
+    """Um assunto que o usuário está aprendendo"""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma string do model"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Devolve uma string do model"""
+        return self.text[:50] + "..."
