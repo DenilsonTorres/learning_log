52852e4617295a479134925d0c9d2e34fc44ac1a
Denilson Torres <dtlima2020@gmail.com>
2024-06-10 21:16:52 -0300

    refactor: added_links

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+    path("topics/<topic_id>/", views.topic, name="topic"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     # Página inicial
     return render(request, "learning_logs/index.html")
+
+
+def topics(request):
+    # Página de topicos
+
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "learning_logs/topics.html", context)
+
+
+def topic(request, topic_id):
+    # Mostra um unico tópico
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
