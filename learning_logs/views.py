577122c41415a57917d5c6897312a74448970768
Denilson Torres <dtlima2020@gmail.com>
2024-06-14 11:17:35 -0300

    function_add: adicionar anotacoes

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import TopicForm, EntryForm
 from .models import Topic
 
 
@@ -21,3 +24,34 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
+
+def new_topic(request):
+    """Adiciona novo assunto"""
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("topics"))
+
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        form = EntryForm()
+
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse("topic", args=[topic_id]))
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
