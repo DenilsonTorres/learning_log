edefc3b0b3c3e648539ac96584a456a8d8f119ee
Denilson Torres <dtlima2020@gmail.com>
2024-06-17 09:45:03 -0300

    added login_screen

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("learning_logs.urls")),
+    path("users/", include("users.urls")),
 ]
