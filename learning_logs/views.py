0f6e7e29e8778393efa7a1327264701ae09619b0
Denilson Torres <dtlima2020@gmail.com>
2024-06-17 09:14:40 -0300

    add_command "editar informações"

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import TopicForm, EntryForm
-from .models import Topic
+from .models import Topic, Entry
 
 
 def index(request):
@@ -55,3 +55,21 @@ def new_entry(request, topic_id):
             return HttpResponseRedirect(reverse("topic", args=[topic_id]))
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    # Editar uma nota existente
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        form = EntryForm(instance=entry)
+
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("topic", args=[topic.id]))
+
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
